a48cb5ec005748048163f46d3ed88cb8ec403281
Philip Urban <urban.philip@googlemail.com>
2013-05-29 18:33:55 +0200

    fixed compass bug

--- src/main/java/de/raidcraft/rcgraveyards/listener/PlayerListener.java
+++ src/main/java/de/raidcraft/rcgraveyards/listener/PlayerListener.java
@@ -43,7 +43,7 @@ public class PlayerListener implements Listener {
         RCGraveyardsPlugin plugin = RaidCraft.getComponent(RCGraveyardsPlugin.class);
         GraveyardPlayer graveyardPlayer = plugin.getPlayerManager().getGraveyardPlayer(event.getEntity().getName());
 
-        graveyardPlayer.getLastDeath().setLocation(event.getEntity().getLocation());
+        graveyardPlayer.getLastDeath().setLocation(event.getEntity().getLocation().clone());
         graveyardPlayer.getLastDeath().setTimestamp(System.currentTimeMillis());
     }
 
@@ -61,7 +61,7 @@ public class PlayerListener implements Listener {
         if(graveyard == null) return;
         event.setRespawnLocation(graveyard.getLocation());
         player.sendMessage("*********************************************************************");
-        player.sendMessage(ChatColor.DARK_GREEN + "Du bist am Friedhof " + ChatColor.GOLD + graveyard.getFriendlyName() + ChatColor.DARK_GREEN + " als Geist respawned.");
+        player.sendMessage(ChatColor.RED + "Du bist am Friedhof " + ChatColor.YELLOW + graveyard.getFriendlyName() + ChatColor.DARK_GREEN + " als Geist respawned.");
         player.sendMessage(ChatColor.GOLD + "Der Kompass zeigt dir den Weg zurück zu deiner Leiche und deinem Inventar.");
         player.sendMessage(ChatColor.GOLD + "Oder nutze den Geisterheiler hier auf dem Friedhof und verliere dadurch Items.");
         player.sendMessage("*********************************************************************");
