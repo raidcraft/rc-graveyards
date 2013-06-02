293e3524bbfbdd0f618d73eb2993d958be8eb160
Philip Urban <urban.philip@googlemail.com>
2013-06-02 20:20:18 +0200

    disabled protocol faking

--- src/main/java/de/raidcraft/rcgraveyards/managers/GhostManager.java
+++ src/main/java/de/raidcraft/rcgraveyards/managers/GhostManager.java
@@ -1,18 +1,7 @@
 package de.raidcraft.rcgraveyards.managers;
 
-import com.comphenix.protocol.Packets;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.ConnectionSide;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedDataWatcher;
-import de.raidcraft.api.packets.Packet28EntityMetadata;
-import de.raidcraft.rcgraveyards.RCGraveyardsPlugin;
 import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Sheep;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
@@ -32,36 +21,36 @@ public class GhostManager implements Listener {
 
         createTask(plugin);
 
-        ProtocolLibrary.getProtocolManager().addPacketListener(
-            new PacketAdapter(plugin, ConnectionSide.SERVER_SIDE, Packets.Server.ENTITY_METADATA) {
-                @Override
-                public void onPacketSending(PacketEvent event) {
-                    if(!isGhost(event.getPlayer())) return;
-                    switch (event.getPacketID()) {
-                        case Packets.Server.ENTITY_METADATA:
-                            Packet28EntityMetadata packet28 = new Packet28EntityMetadata(event.getPacket());
-                            Entity entity = packet28.getEntity(event);
-                            if(entity.hasMetadata(RCGraveyardsPlugin.VISIBLE_FOR_GHOSTS_METADATA)) return;
-
-                            WrappedDataWatcher watcher = new WrappedDataWatcher(packet28.getEntityMetadata());
-                            Byte flag = watcher.getByte(0);
-                            if (flag != null) {
-                                packet28 = new Packet28EntityMetadata(packet28.getHandle().deepClone());
-                                watcher = new WrappedDataWatcher(packet28.getEntityMetadata());
-
-                                if(entity instanceof Sheep) {
-                                    Byte woolState = watcher.getByte(16);
-                                    watcher.setObject(16, (byte) (woolState | 16));
-                                }
-
-                                watcher.setObject(0, (byte) (flag | 32));
-                                event.setPacket(packet28.getHandle());
-                            }
-                            break;
-                    }
-                }
-            }
-        );
+//        ProtocolLibrary.getProtocolManager().addPacketListener(
+//            new PacketAdapter(plugin, ConnectionSide.SERVER_SIDE, Packets.Server.ENTITY_METADATA) {
+//                @Override
+//                public void onPacketSending(PacketEvent event) {
+//                    if(!isGhost(event.getPlayer())) return;
+//                    switch (event.getPacketID()) {
+//                        case Packets.Server.ENTITY_METADATA:
+//                            Packet28EntityMetadata packet28 = new Packet28EntityMetadata(event.getPacket());
+//                            Entity entity = packet28.getEntity(event);
+//                            if(entity.hasMetadata(RCGraveyardsPlugin.VISIBLE_FOR_GHOSTS_METADATA)) return;
+//
+//                            WrappedDataWatcher watcher = new WrappedDataWatcher(packet28.getEntityMetadata());
+//                            Byte flag = watcher.getByte(0);
+//                            if (flag != null) {
+//                                packet28 = new Packet28EntityMetadata(packet28.getHandle().deepClone());
+//                                watcher = new WrappedDataWatcher(packet28.getEntityMetadata());
+//
+//                                if(entity instanceof Sheep) {
+//                                    Byte woolState = watcher.getByte(16);
+//                                    watcher.setObject(16, (byte) (woolState | 16));
+//                                }
+//
+//                                watcher.setObject(0, (byte) (flag | 32));
+//                                event.setPacket(packet28.getHandle());
+//                            }
+//                            break;
+//                    }
+//                }
+//            }
+//        );
     }
 
     private void createTask(Plugin plugin) {
@@ -147,9 +136,5 @@ public class GhostManager implements Listener {
 //            }
 //        } catch (InvocationTargetException e) {
 //        }
-
-        Chunk chunk = player.getLocation().getChunk();
-        chunk.unload();
-        chunk.load();
     }
 }
